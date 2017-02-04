40559a7b93e275a874bd5ff19c818166959d8d2f
David Yunker <David Yunker>
2017-02-04 12:19:16 -0600

    boilerplate for new front-end project

---
+++ MyBangazonFrontEnd/app/Controllers/ToDoCtrl.js
@@ -0,0 +1,84 @@
+﻿"use strict";
+
+
+app.controller("ToDoCtrl", function ($scope, $location, ApiCallFactory, $routeParams) {
+
+    $scope.ToDoInfo = {
+        Id: "",
+        Name: "",
+        Description: "",
+        Status: "",
+        CompletedOn: ""
+
+    };
+
+
+    // Create a simple Angular frontend.
+    // Users should be able to create a new task.
+    // Users should be able to update a task's Name, Description and Status.
+    // Validation should be performed.
+    // Users should be able to view a list of tasks by Status.
+    // Users should be able to view a list of tasks by CompletedOn.
+
+    $scope.loadAllToDos = function () {
+        console.log("loadAllToDos has started!")
+        ApiCallFactory.getToDos()
+          .then(function (data) {
+              console.log("it worked!")
+              console.log("data", data);
+              $scope.ToDoInfo = data
+          })
+    }
+
+    $scope.getOneToDo = function () {
+        console.log("getOneToDo is running")
+        console.log($scope.ToDoInfo, "ToDoInfo")
+        ApiCallFactory.getSingleToDo($routeParams.id)
+          .then(function (data) {
+              console.log("data", data);
+          })
+
+    }
+
+    TaskFactory.getSingleTask($routeParams.userTaskId)
+      .then((task) => {
+          console.log("task", task);
+          $scope.task = task;
+      });
+
+    $scope.AddNewToDo = function () {
+        console.log("AddNewToDo has started")
+        ApiCallFactory.postToDo($scope.ToDoInfo)
+          .then(function (data) {
+              console.log("it worked!")
+              console.log("data", data)
+
+          })
+
+
+    }
+
+
+
+    $scope.GetToDoByStatus = function () {
+        console.log("getToDoByStatus running")
+        ApiCallFactory.toDoByStatus(id)
+          .then(function (data) {
+              console.log("it worked")
+              console.log("data", data)
+          })
+
+    }
+
+
+    $scope.EditToDo = function () {
+        console.log("EditToDo running")
+        ApiCallFactory.putToDo(ToDoInfo, id)
+          .then(function (data) {
+              console.log("it worked")
+              console.log("data", data)
+          })
+
+    }
+
+})
---
+++ MyBangazonFrontEnd/app/Factories/ApiCallFactory.js
@@ -0,0 +1,94 @@
+﻿"use strict";
+
+
+app.factory("ApiCallFactory", ($q, $http, $location) => {
+
+
+    let getToDos = () => {
+        return $q((resolve, reject) => {
+            $http.get(`http://localhost:5000/api/values`)
+            .success((itemObject) => {
+                resolve(itemObject.data)
+            })
+            .error((error) => {
+                reject(error)
+                console.log("error", error)
+            })
+        })
+    }
+
+    let getSingleToDo = (id) => {
+        return $q((resolve, reject) => {
+            $http.get(`http://localhost:5000/api/values/${id}`)
+            .success((itemObject) => {
+                resolve(itemObject);
+            })
+            .error((error) => {
+                reject(error);
+                console.log("error", error)
+            })
+        })
+    }
+
+
+
+
+    let toDoByStatus = (id) => {
+        return $q((resolve, reject) => {
+            $http.get(`http://localhost:5000/api/values/ByStatus/${id}`)
+            .success((itemObject) => {
+                resolve(itemObject);
+            })
+            .error((error) => {
+                reject(error);
+                console.log("error", error)
+            });
+        });
+    };
+
+    let postToDo = (ToDoInfo) => {
+        console.log("postToDo is running", ToDoInfo)
+        return $q((resolve, reject) => {
+            $http.post(`http://localhost:5000/api/values`,
+             JSON.stringify(ToDoInfo))
+             .then((ObjFromMyServer) => {
+                 console.log(ObjFromMyServer);
+                 resolve(ObjFromMyServer);
+             })
+             .catch((error) => {
+                 reject(error);
+             })
+        })
+
+    }
+
+
+
+    let putToDo = (ToDoInfo, id) => {
+        console.log("putToDo is running", id)
+        return $q((resolve, reject) => {
+            $http.put(`http://localhost:5000/api/values/${id}`,
+             JSON.stringify(ToDoInfo))
+             .success((ObjFromMyServer) => {
+                 resolve(ObjFromMyServer);
+             })
+             .error((error) => {
+                 reject(error);
+             })
+        })
+
+
+    }
+    let deleteToDo = (id) => {
+        console.log("deleteToDo is running", id)
+        return $q((resolve, reject) => {
+            $http.delete(`http://localhost:5000/api/values/${id}`)
+            .success((ObjFromMyServer) => {
+                console.log("this is the ObjFromMyServer", ObjFromMyServer)
+                resolve(ObjFromMyServer);
+            })
+        })
+    }
+
+    return { putToDo, deleteToDo, postToDo, getToDos, getSingleToDo, putToDo, toDoByStatus }
+})
